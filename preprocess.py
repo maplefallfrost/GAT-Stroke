c629bbea6c5a73dd50b9ea6c2859676388d0f057
Junyu Ye <lkjslkjdlk@163.com>
2019-02-21 15:40:41 +0800

    add generate_time_edge function for generating edge files

--- preprocess.py
+++ preprocess.py
@@ -19,7 +19,7 @@ def dist_remove(strokes, height=128):
         if len(s) == 0:
             continue
         stroke = [s[0], s[1]]
-        for i in xrange(2, len(s), 2):
+        for i in range(2, len(s), 2):
             delta_x = (s[i] - stroke[-2])*ratio
             delta_y = (s[i+1] - stroke[-1])*ratio
             dist = (delta_x**2 + delta_y**2)**0.5
@@ -35,7 +35,7 @@ def coordinate_norm(strokes):
         return ((x2-x1)**2 + (y2-y1)**2)**0.5
     sum_L, sum_px, sum_py = 0, 0, 0
     for s in strokes:
-        for i in xrange(0, len(s) - 2, 2):
+        for i in range(0, len(s) - 2, 2):
             L = line_len(*s[i:i+4])
             sum_L += L
             sum_px += 0.5 * L * (s[i] + s[i+2])
@@ -45,7 +45,7 @@ def coordinate_norm(strokes):
 
     sum_dx = 0
     for s in strokes:
-        for i in xrange(0, len(s) - 2, 2):
+        for i in range(0, len(s) - 2, 2):
             L = line_len(*s[i:i+4])
             dx = 1.0 / 3 * L * \
                  ((s[i+3]-mu_x)**2 + (s[i]-mu_x)**2 +
@@ -55,7 +55,7 @@ def coordinate_norm(strokes):
     res = []
     for s in strokes:
         stroke = []
-        for i in xrange(0, len(s), 2):
+        for i in range(0, len(s), 2):
             stroke.append((s[i] - mu_x) / sigma_x)
             stroke.append((s[i+1] - mu_y) / sigma_x)
         res.append(stroke)
@@ -65,7 +65,7 @@ def coordinate_norm(strokes):
 def extract_online_feature(strokes, save_dir):
     fp = open(save_dir, 'w')
     for s in strokes:
-        for i in xrange(0, len(s), 2):
+        for i in range(0, len(s), 2):
             delta_x = s[i] - s[i-2] if i >= 2 else 0
             delta_y = s[i+1] - s[i-1] if i >= 2 else 0
             delta2_x = s[i] - s[i-4] if i >= 4 else 0
@@ -79,7 +79,7 @@ def extract_online_feature(strokes, save_dir):
 def from_numpy_to_list(numpy_strokes):
     strokes = []
     stroke = []
-    for i in xrange(numpy_strokes.shape[0]):
+    for i in range(numpy_strokes.shape[0]):
         stroke.append(numpy_strokes[i][0])
         stroke.append(numpy_strokes[i][1])
         if numpy_strokes[i][2] == 1:
@@ -117,15 +117,24 @@ def read_data(data_dir):
             print(len(tag_seqs[-1]))
         
     return point_seqs, tag_seqs
+
+def generate_time_edge(list_strokes, save_dir):
+    num_node = len(list_strokes)
+    with open(save_dir, 'w') as fp:
+        for i in range(num_node - 1):
+            fp.write("%d %d\n" % (i, i + 1))
     
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage python preprocess.py (train/valid/test)")
+    if len(sys.argv) != 3:
+        print("Usage python preprocess.py (train/valid/test) (f/e)")
         sys.exit(0)
     home = os.environ['HOME']
-    data_path = os.path.join(home, "data/IAMonDo/raw_multi_cls")
+    data_path = os.path.join(home, "data/IAMonDo/cls_5class")
     data_path = os.path.join(data_path, sys.argv[1])
+    operation = sys.argv[2]
+    
+    # from points to point features
     all_point_feature = [x for x in os.listdir(data_path) if x.endswith('.point_feature')]
     for f in all_point_feature:
         file_name = f.split(".")[0]
@@ -133,7 +142,12 @@ if __name__ == "__main__":
         save_dir = os.path.join(data_path, file_name + ".raw_feature")
         numpy_strokes = np.loadtxt(read_dir, dtype=np.float32)
         list_strokes = from_numpy_to_list(numpy_strokes)
-        dist_remove_strokes = dist_remove(list_strokes)
-        norm_strokes = coordinate_norm(dist_remove_strokes)
-        extract_online_feature(norm_strokes, save_dir)
         
+        if 'f' in operation:
+            dist_remove_strokes = dist_remove(list_strokes)
+            norm_strokes = coordinate_norm(dist_remove_strokes)
+            extract_online_feature(norm_strokes, save_dir)
+        
+        if 'e' in operation:
+            time_save_dir = os.path.join(data_path, file_name + ".time_edge")
+            generate_time_edge(list_strokes, time_save_dir)
