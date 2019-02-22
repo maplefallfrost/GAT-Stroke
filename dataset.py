1f6d84d74266b3048db510e974d612403af3be6c
Junyu Ye <lkjslkjdlk@163.com>
2019-02-22 15:28:01 +0800

    change the label organization from torch.LongTensor to dgl graph for easy training and evaluation

--- dataset.py
+++ dataset.py
@@ -6,14 +6,12 @@ import dgl
 
 def collate(device):
     def collate_dev(samples):
-        graphs, labels, seq_len = map(list, zip(*samples))
-        batched_graph = dgl.batch(graphs)
-        max_len = max(map(len, labels))
-        batch_size = len(labels)
-        pad_labels = th.zeros(batch_size, max_len)
-        for i, l in enumerate(labels):
-            pad_labels[i, :seq_len[i]] = th.tensor(labels[i])
-        return batched_graph, pad_labels.to(device), th.tensor(seq_len).long().to(device)
+        feature_graphs, label_graphs = map(list, zip(*samples))
+        batched_feature_graphs = dgl.batch(feature_graphs)
+        batched_label_graphs = dgl.batch(label_graphs)
+        batched_feature_graphs.ndata['x'] = batched_feature_graphs.ndata['x'].to(device)
+        batched_label_graphs.ndata['y'] = batched_label_graphs.ndata['y'].to(device)
+        return batched_feature_graphs, batched_label_graphs
     return collate_dev
 
 class StrokeDataset(object):
@@ -35,7 +33,8 @@ class StrokeDataset(object):
         self.labels = self._get_content(self.label_files)
         self.time_edges = self._get_content(self.time_edge_files)
         
-        self.graphs = []
+        self.feature_graphs = []
+        self.label_graphs = []
         self._gen_graph()
     
     def _get_content(self, files):
@@ -46,13 +45,20 @@ class StrokeDataset(object):
         return all_content
     
     def _gen_graph(self):
-        for f, e in zip(self.stroke_features, self.time_edges):
-            g = dgl.DGLGraph()
-            g.add_nodes(f.shape[0])
-            g.ndata['x'] = th.Tensor(f)
+        for f, l, e in zip(self.stroke_features, self.labels, self.time_edges):
+            feature_graph = dgl.DGLGraph()
+            feature_graph.add_nodes(f.shape[0])
+            feature_graph.ndata['x'] = th.FloatTensor(f)
             src, dst = map(list, zip(*e))
-            g.add_edges(src, dst)
-            self.graphs.append(g)
+            feature_graph.add_edges(src, dst)
+            self.feature_graphs.append(feature_graph)
+            
+            label_graph = dgl.DGLGraph()
+            assert f.shape[0] == l.shape[0]
+            label_graph.add_nodes(l.shape[0])
+            label_graph.ndata['y'] = th.LongTensor(l)
+            label_graph.add_edges(src, dst)
+            self.label_graphs.append(label_graph)
     
     def __len__(self):
         """Return the number of samples in the dataset."""
@@ -67,19 +73,8 @@ class StrokeDataset(object):
            The sample index.
         Returns
         -------
-        (dgl.DGLGraph, numpy.ndarray, int)
-            The graph, labels and actual length.
+        (dgl.DGLGraph, dgl.DGLGraph)
+            The feature graph and label graph
         """
-        return self.graphs[idx], self.labels[idx], len(self.labels[idx])
-
-if __name__ == "__main__":
-    home = os.environ['HOME']
-    data_dir = os.path.join(home, "data/IAMonDo/cls_5class")
-    trainset = StrokeDataset(os.path.join(data_dir, "train"))
-    
-    train_loader = DataLoader(trainset, batch_size=10, shuffle=True, collate_fn=collate)
-    for it, (bg, label, seq_len) in enumerate(data_loader):
-        print(label.size())
-        print(seq_len)
-        break
+        return self.feature_graphs[idx], self.label_graphs[idx]
     
