5784a04e5a29da4b72c7b3ca45fd85fe7551253e
Junyu Ye <maplefallfrost@gmail.com>
2019-02-24 00:55:31 +0800

    reorganize some codes and print more information

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import argparse
+import time
 
 from dataset import StrokeDataset, collate
 from torch.utils.data import DataLoader
@@ -43,9 +44,9 @@ def train(args):
     testset = StrokeDataset(data_dir, edge_dir, "test", num_classes)
         
     train_loader = DataLoader(trainset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          collate_fn=collate(device))
+                              batch_size=batch_size,
+                              shuffle=True,
+                              collate_fn=collate(device))
 
     valid_loader = DataLoader(validset,
                               batch_size=batch_size,
@@ -59,8 +60,17 @@ def train(args):
     
     heads = ([num_heads] * num_layers) + [num_out_heads]
 
-    model = GAT(num_layers, node_f_dim, edge_f_dim, num_hidden, num_classes,
-                heads, F.elu, in_drop, attn_drop, alpha, residual).to(device)
+    model = GAT(num_layers,
+                node_f_dim,
+                edge_f_dim,
+                num_hidden,
+                num_classes,
+                heads,
+                nn.LeakyReLU(alpha),
+                in_drop,
+                attn_drop,
+                alpha,
+                residual).to(device)
     loss_func = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=10)
@@ -70,8 +80,10 @@ def train(args):
     best_test_acc = 0
     best_round = 0
 
+    start = time.time()
     for epoch in range(epochs):
         epoch_loss = 0
+        epoch_start = time.time()
         for it, (fg, lg) in enumerate(train_loader):
             logits = model(fg)
             labels = lg.ndata['y']
@@ -82,28 +94,32 @@ def train(args):
             epoch_loss += loss.detach().item()
 
         epoch_loss /= (it + 1)
-        print('Epoch {:3d}, loss {:4f}'.format(epoch, epoch_loss))
+        epoch_duration = time.time() - epoch_start
+        print('Epoch: {:3d}, loss: {:4f}, speed: {:.2f}doc/s'.format(
+            epoch, epoch_loss, len(trainset) / epoch_duration))
         epoch_losses.append(epoch_loss)
 
         train_acc, _= evaluate(model, train_loader, num_classes, "train")
         valid_acc, _ = evaluate(model, valid_loader, num_classes, "valid")
         if valid_acc > best_valid_acc:
+            best_valid_acc = valid_acc
             test_acc, test_conf_mat = evaluate(model, test_loader, num_classes, "test")
-            if test_acc > best_test_acc:
-                best_test_acc = test_acc
-                best_conf_mat = test_conf_mat
-                best_round = epoch
+            best_conf_mat = test_conf_mat
+            best_round = epoch
                 
         scheduler.step(valid_acc)
         cur_learning_rate = optimizer.param_groups[0]['lr']
         print('Learning rate: {:10f}'.format(cur_learning_rate))
+        epoch_duration = time.time() - epoch_start
         if cur_learning_rate <= 1e-6:
             break
 
     print("Best round: %d" % best_round)
     print_result(best_conf_mat)
+    duration = time.time() - start
+    print("Time cost: {:.4f}".format(duration))
     
-    return best_test_acc
+    return test_acc
 
 if __name__ == "__main__":
     
@@ -118,7 +134,7 @@ if __name__ == "__main__":
                         help="which GPU to use. Set -1 to use CPU.")
     parser.add_argument("--epochs", type=int, default=100,
                         help="number of training epochs")
-    parser.add_argument("--batch_size", type=int, default=32,
+    parser.add_argument("--batch_size", type=int, default=16,
                         help="batch size")
     parser.add_argument("--num_heads", type=int, default=8,
                         help="number of hidden attention heads")
@@ -130,7 +146,7 @@ if __name__ == "__main__":
                         help="number of hidden units")
     parser.add_argument("--residual", action="store_true", default=True,
                         help="use residual connection")
-    parser.add_argument("--in_drop", type=float, default=.0,
+    parser.add_argument("--in_drop", type=float, default=.2,
                         help="input feature dropout")
     parser.add_argument("--attn_drop", type=float, default=.2,
                         help="attention dropout")
